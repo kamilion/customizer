ae25cd476b085bdc52c549f35fceab74cf9e210a
Ivailo Monev <xakepa10@gmail.com>
2011-11-27 19:29:42 -0500

    Removed old initctl fix

--- scripts/Archive.sh
+++ scripts/Archive.sh
@@ -74,8 +74,6 @@ Yellow='\e[1;33m'
 echo -e "${Yellow}   *${Reset} ${Green}Setting up${Reset}"
 export HOME=/root
 export LC_ALL=C
-dpkg-divert --local --rename --add /sbin/initctl
-ln -f -s /bin/true /sbin/initctl
 
 echo -e "${Yellow}   *${Reset} ${Green}Making sure everything is configured${Reset}"
 dpkg --configure -a
@@ -90,11 +88,9 @@ echo -e "${Yellow}   *${Reset} ${Green}Running package manager:${Reset} ${Yellow
 
 echo -e "${Yellow}   *${Reset} ${Green}Cleaning up${Reset}"
 apt-get clean
-dpkg-divert --remove /sbin/initctl
 rm -f /etc/debian_chroot
 rm -f /etc/hosts
 rm -f /etc/resolv.conf
-rm -f /sbin/initctl
 rm -f ~/.bash_history
 rm -f /boot/*.bak
 rm -f /var/lib/dpkg/*-old
