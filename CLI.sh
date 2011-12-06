b24d94464867e1605b4be0a831f1d49d5b93e0a4
Ivailo Monev <xakepa10@gmail.com>
2011-12-06 02:49:31 +0200

    Removed test lines for log

--- CLI.sh
+++ CLI.sh
@@ -24,10 +24,6 @@ set -e
 source /opt/Customizer/common
 source /opt/Customizer/settings.conf
 
-exec >/tmp/foo.log
-tail -f /tmp/foo.log &
-
-
 Root_it() {
 	if [ "$UID" != "0" ];then
 		WARNING_MESSAGE "You are not root! Promting for password"
