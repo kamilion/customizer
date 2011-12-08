e3f2af7337c7cd46d7ef2a3ccbb1ec3b2bd4141c
Ivailo Monev <xakepa10@gmail.com>
2011-12-09 01:23:22 +0200

    Merged with devel

--- CLI.sh
+++ CLI.sh
@@ -1,23 +1,37 @@
 #!/usr/bin/env bash
+# Customizer - Advanced LiveCD Remastering Tool
+# Copyright (C) 2011  Ivailo Monev
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
-# This script is part of Customzier
-#
-# Homepage: http://u-customizer.sf.net
-# Forum: https://sourceforge.net/apps/phpbb/u-customizer
-#
-# C0diNg: Ivailo Monev (a.k.a SmiL3y)
-#
-# E-Mail: xakepa10@gmail.com
-# Jabber: xakepa@jabber.org
-# Skype: big-smoke10
+# Homepage: https://github.com/fluxer/Customizer
+# Wiki: https://github.com/fluxer/Customizer/wiki
+# Issues: https://github.com/fluxer/Customizer/issues
 #
 set -e
-source /opt/Customizer/Functions.sh
+source /opt/Customizer/common
 source /opt/Customizer/settings.conf
-Reset='\e[0m'
-Red='\e[1;31m'
-Green='\e[1;32m'
-Yellow='\e[1;33m'
+
+Root_it() {
+	if [ "$UID" != "0" ];then
+		WARNING_MESSAGE "You are not root! Promting for password"
+		su -c "$1"
+    else
+        "$1"
+	fi
+}
 
 Usage () {
 echo "
@@ -27,7 +41,7 @@ echo "
      -c|--chroot    Chroot into the filesystem
      -x|--xnest     Execute nested X-session
      -e|--sources   Edit sources.list
-     -a|--archive   Execute package manager
+     -p|--pkgm      Execute package manager
      -d|--deb       Install Debian package
      -k|--hook      Execute hook
      -g|--gui       Install GUI (DE/WM)
@@ -38,48 +52,58 @@ echo "
      -t|--clean     Clean all temporary files and folders
 
  Other options:
+     
      -h|--help      Display this message
      -v|--version   Show the current version and more
 "
 }
 
 Version () {
-echo
-echo "Customizer v3.0.7
+echo "
+Customizer v3.1.0
 
-Homepage: http://u-customizer.sf.net
-Forum: https://sourceforge.net/apps/phpbb/u-customizer
+Links:
+
+  Homepage: https://github.com/fluxer/Customizer
+  Wiki: https://github.com/fluxer/Customizer/wiki
+  Issues: https://github.com/fluxer/Customizer/issues
 
 
-Main developer:
-Ivailo Monev (a.k.a SmiL3y)
-<xakepa10@gmail.com>
+Credits:
 
-PPA maintainer:
-Michał Głowienka (a.k.a. eloaders)
-<eloaders@yahoo.com>"
-echo
+  Main developer:
+    Ivailo Monev (a.k.a SmiL3y)
+    <xakepa10@gmail.com>
+
+  PPA maintainer:
+    Michał Głowienka (a.k.a. eloaders)
+    <eloaders@yahoo.com>
+
+  Documentation:
+    Mubiin Kimura (a.k.a. clearkimura)
+    <clearkimura@gmail.com>
+"
 }
 
 if [ "$#" != "0" ];then
 	for arg in "$@"; do
 		case $arg in
-			-u|--use) su -c /opt/Customizer/scripts/Use.sh ;;
-			-c|--chroot) su -c /opt/Customizer/scripts/Chroot.sh ;;
-			-x|--xnest) su -c /opt/Customizer/scripts/Xnest.sh ;;
-			-e|--sources) su -c /opt/Customizer/scripts/EditSources.sh ;;
-			-a|--archive) su -c /opt/Customizer/scripts/Archive.sh ;;
-			-d|--deb) su -c /opt/Customizer/scripts/InstallDEB.sh ;;
-			-k|--hook) su -c /opt/Customizer/scripts/Hook.sh ;;
-			-g|--gui) su -c /opt/Customizer/scripts/InstallGUI.sh ;;
-			-s|--snapshot) su -c /opt/Customizer/scripts/CreateSnapshot.sh ;;
-			-i|--import) su -c /opt/Customizer/scripts/ImportSnapshot.sh ;;
-			-r|--rebuild) su -c /opt/Customizer/scripts/Build.sh ;;
-			-q|--qemu) su -c /opt/Customizer/scripts/QEMU.sh ;;
-			-t|--clean) su -c /opt/Customizer/scripts/Clean.sh ;;
-			-v|--version) Version ;;
+			-u|--use) Root_it /opt/Customizer/scripts/extract ;;
+			-c|--chroot) Root_it /opt/Customizer/scripts/chroot ;;
+			-x|--xnest) Root_it /opt/Customizer/scripts/xnest ;;
+			-e|--sources) Root_it /opt/Customizer/scripts/edit_sources ;;
+			-p|--pkgm) Root_it /opt/Customizer/scripts/package_manager ;;
+			-d|--deb) Root_it /opt/Customizer/scripts/install_deb ;;
+			-k|--hook) Root_it /opt/Customizer/scripts/hook ;;
+			-g|--gui) Root_it /opt/Customizer/scripts/install_gui ;;
+			-s|--snapshot) Root_it /opt/Customizer/scripts/create_snapshot ;;
+			-i|--import) Root_it /opt/Customizer/scripts/import_snapshot ;;
+			-r|--rebuild) Root_it /opt/Customizer/scripts/rebuild ;;
+			-q|--qemu) Root_it /opt/Customizer/scripts/qemu ;;
+			-t|--clean) Root_it /opt/Customizer/scripts/clean ;;
+            -v|--version) Version ;;
 			-h|--help) Usage ;;
-			*) echo -e "${Red}ERROR${Reset}: ${Yellow}Unrecognized argument${Reset}: ${Green}$arg${Reset}" ;;
+			*) EXTRA_ERROR_MESSAGE "Unrecognized argument" "$arg" ;;
 		esac
 	done
 else
