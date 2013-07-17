fb28171f3c429c368ff8c301dc5653d1b9cba2f2
Ivailo Monev <xakepa10@gmail.com>
2013-07-17 13:52:26 +0000

    make use of getboolean

--- src/lib/configparser.py
+++ src/lib/configparser.py
@@ -7,10 +7,10 @@ import lib.message as message
 FILESYSTEM_DIR = "/home/FileSystem"
 ISO_DIR = "/home/ISO"
 MOUNT_DIR = "/media"
-MESSAGES_COLORS = "True"
-FORCE_CHROOT = "True"
+MESSAGES_COLORS = True
+FORCE_CHROOT = True
 LOCALES = "C"
-CHROOT_HELPER = "True"
+CHROOT_HELPER = True
 RESOLUTION = "800x600"
 COMPRESSION = "xz"
 BOOT_FILES = "False"
@@ -26,10 +26,10 @@ FILESYSTEM_DIR = conf.get('main', 'FILESYSTEM_DIR')
 ISO_DIR = conf.get('main', 'ISO_DIR')
 MOUNT_DIR = conf.get('main', 'MOUNT_DIR')
 
-MESSAGES_COLORS = conf.get('preferences', 'MESSAGES_COLORS')
-FORCE_CHROOT = conf.get('preferences', 'FORCE_CHROOT')
+MESSAGES_COLORS = conf.getboolean('preferences', 'MESSAGES_COLORS')
+FORCE_CHROOT = conf.getboolean('preferences', 'FORCE_CHROOT')
 LOCALES = conf.get('preferences', 'LOCALES')
-CHROOT_HELPER = conf.get('preferences', 'CHROOT_HELPER')
+CHROOT_HELPER = conf.getboolean('preferences', 'CHROOT_HELPER')
 RESOLUTION = conf.get('preferences', 'RESOLUTION')
 COMPRESSION = conf.get('preferences', 'COMPRESSION')
 BOOT_FILES = conf.get('preferences', 'BOOT_FILES')
