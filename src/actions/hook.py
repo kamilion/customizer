f67ebaa965811e11ae724340b0a0fd7061d77f12
Ivailo Monev <xakepa10@gmail.com>
2013-07-18 00:53:44 +0000

    hook should work now

---
+++ src/actions/hook.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python2
+
+import sys, os, shutil, stat, subprocess
+
+import lib.misc as misc
+import lib.configparser as configparser
+import lib.message as message
+
+def check():
+	if not os.path.isfile(configparser.HOOK):
+		message.mark_sub_critical('HOOK does not exists', configparser.HOOK)
+		sys.exit(2)
+		
+def main():
+	message.sub_info('Checking')
+	check()
+	
+	message.sub_info('Copying HOOK file')
+	hook_file = misc.join_paths(configparser.FILESYSTEM_DIR, 'hook')
+	if os.path.isfile(deb_file):
+		os.unlink(deb_file)
+	misc.copy_file(configparser.HOOK, hook_file)
+	
+	message.sub_info('Making HOOK executable')
+	os.chmod(hook_file, stat.S_IEXEC)
+	
+	message.sub_info('Running HOOK')
+	misc.chroot_exec(['exec', '/hook'])
+	
