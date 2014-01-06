220de584a42d6c7752f5692b5e2246fbbbd3511f
Ivailo Monev <xakepa10@gmail.com>
2014-01-06 02:36:23 +0000

    Update extract.py

--- src/actions/extract.py
+++ src/actions/extract.py
@@ -7,6 +7,14 @@ import lib.config as config
 import lib.message as message
 import actions.common as common
 
+# TODO: this is not the best way to do it, exception can be raised at any time
+def unmount_iso():
+    message.sub_info('Unmounting', mount_dir)
+    subprocess.check_call((misc.whereis('umount'), mount_dir))
+
+    message.sub_info('Removing', mount_dir)
+    os.rmdir(mount_dir)
+
 def main():
     if not os.path.isfile(config.ISO):
         message.sub_critical('ISO does not exists', config.ISO)
@@ -30,16 +38,21 @@ def main():
         if not os.path.exists(sfile):
             message.sub_critical('Invalid ISO', config.ISO)
             common.clean_work_dirs()
+            unmount_iso()
             sys.exit(2)
 
     message.sub_info('Unsquashing filesystem')
-    subprocess.check_call((misc.whereis('unsquashfs'), '-f', '-d', config.FILESYSTEM_DIR, mount_dir + '/casper/filesystem.squashfs'))
+    try:
+        subprocess.check_call((misc.whereis('unsquashfs'), '-f', '-d', config.FILESYSTEM_DIR, mount_dir + '/casper/filesystem.squashfs'))
+    except:
+        unmount_iso()
 
     message.sub_info('Checking architecture')
     architecture = misc.chroot_exec(('dpkg', '--print-architecture'), prepare=False, mount=False, output=True)
     if architecture == 'amd64' and not os.uname()[4] == 'x86_64':
         message.sub_critical('The ISO architecture is amd64 and yours is not')
         common.clean_work_dirs()
+        unmount_iso()
         sys.exit(2)
 
     message.sub_info('Copying ISO files')
@@ -51,8 +64,4 @@ def main():
         else:
             misc.copy_file(sfile, sfile.replace(mount_dir, config.ISO_DIR))
 
-    message.sub_info('Unmounting', mount_dir)
-    subprocess.check_call((misc.whereis('umount'), mount_dir))
-
-    message.sub_info('Removing', mount_dir)
-    os.rmdir(mount_dir)
+    unmount_iso()
