baf1fd6c6b0a6ed692c6f6bf2691cb6709ffea3f
Ivailo Monev <xakepa10@gmail.com>
2015-01-13 14:45:58 +0000

    support running apper

--- src/actions/pkgm.py
+++ src/actions/pkgm.py
@@ -11,7 +11,7 @@ def main():
     common.check_filesystem()
 
     pkgmngr = None
-    for sfile in ('aptitude', 'aptitude-curses', 'synaptic'):
+    for sfile in ('aptitude', 'aptitude-curses', 'synaptic', 'apper'):
         for sdir in ('bin', 'sbin', 'usr/bin', 'usr/sbin'):
             full_file = misc.join_paths(config.FILESYSTEM_DIR, sdir, sfile)
             if os.path.exists(full_file) and os.access(full_file, os.X_OK):
@@ -21,5 +21,17 @@ def main():
     if not pkgmngr:
         raise(message.exception('No package manager available'))
 
-    message.sub_info('Executing package manager')
-    misc.chroot_exec((pkgmngr))
+    if not pkgmngr.startswith('aptitude'):
+        try:
+            message.sub_info('Allwoing local access to X-server')
+            misc.system_command((misc.whereis('xhost'), '+local:'))
+
+            message.sub_info('Executing package manager')
+            misc.chroot_exec((pkgmngr))
+
+        finally:
+            message.sub_info('Blocking local access to X-server')
+            misc.system_command((misc.whereis('xhost'), '-local:'))
+    else:
+        message.sub_info('Executing package manager')
+        misc.chroot_exec((pkgmngr))
