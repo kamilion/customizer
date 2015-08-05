97ece5be58b41170ce30f9506be79f4e1723d87d
Ivailo Monev <xakepa10@gmail.com>
2015-08-05 15:58:42 +0300

    add Korean translation on behalf of Minwook Shin

---
+++ tr/customizer_ko_KR.ts
@@ -0,0 +1,318 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0" language="ko_KR" sourcelanguage="">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="gui_ui.py" line="215"/>
+        <source>Customizer GUI</source>
+        <translation>커스터마이저 GUI</translation>
+    </message>
+    <message>
+        <location filename="gui_ui.py" line="216"/>
+        <source>Select ISO</source>
+        <translation>ISO 선택</translation>
+    </message>
+    <message>
+        <location filename="gui_ui.py" line="217"/>
+        <source>Rebuild ISO</source>
+        <translation type="unfinished">ISO 다시빌드</translation>
+    </message>
+    <message>
+        <location filename="gui_ui.py" line="218"/>
+        <source>QEMU</source>
+        <translation>QEMU</translation>
+    </message>
+    <message>
+        <location filename="gui_ui.py" line="219"/>
+        <source>Clean</source>
+        <translation>청소</translation>
+    </message>
+    <message>
+        <location filename="gui_ui.py" line="220"/>
+        <source> Customization </source>
+        <translation>사용자 지정</translation>
+    </message>
+    <message>
+        <location filename="gui_ui.py" line="221"/>
+        <source>Edit sources</source>
+        <translation>소스 수정</translation>
+    </message>
+    <message>
+        <location filename="gui_ui.py" line="222"/>
+        <source>Archive</source>
+        <translation>아카이브</translation>
+    </message>
+    <message>
+        <location filename="gui_ui.py" line="223"/>
+        <source>Install DEB</source>
+        <translation>DEB 설치</translation>
+    </message>
+    <message>
+        <location filename="gui_ui.py" line="224"/>
+        <source>Terminal</source>
+        <translation>터미널</translation>
+    </message>
+    <message>
+        <location filename="gui_ui.py" line="225"/>
+        <source>Desktop</source>
+        <translation>데스크탑</translation>
+    </message>
+    <message>
+        <location filename="gui_ui.py" line="226"/>
+        <source>Execute hook</source>
+        <translation>후크 실행</translation>
+    </message>
+    <message>
+        <location filename="gui_ui.py" line="227"/>
+        <source> Configuration </source>
+        <translation>구성</translation>
+    </message>
+    <message>
+        <location filename="gui_ui.py" line="228"/>
+        <source>Hostname</source>
+        <translation>호스트 이름</translation>
+    </message>
+    <message>
+        <location filename="gui_ui.py" line="229"/>
+        <source>Live user</source>
+        <translation>라이브 유저</translation>
+    </message>
+    <message>
+        <location filename="gui_ui.py" line="230"/>
+        <source>Main</source>
+        <translation>메인</translation>
+    </message>
+    <message>
+        <location filename="gui_ui.py" line="235"/>
+        <source>X-nest resolution</source>
+        <translation>Х-нест 해상도</translation>
+    </message>
+    <message>
+        <location filename="gui_ui.py" line="236"/>
+        <source>Change</source>
+        <translation>변경</translation>
+    </message>
+    <message>
+        <location filename="gui_ui.py" line="237"/>
+        <source>Working directory</source>
+        <translation>작업 디렉토리</translation>
+    </message>
+    <message>
+        <location filename="gui_ui.py" line="238"/>
+        <source>QEMU RAM (in MB)</source>
+        <translation>QEMU 램 (in MB)</translation>
+    </message>
+    <message>
+        <location filename="gui_ui.py" line="239"/>
+        <source>C</source>
+        <translation>C</translation>
+    </message>
+    <message>
+        <location filename="gui_ui.py" line="240"/>
+        <source>Locales</source>
+        <translation>위치</translation>
+    </message>
+    <message>
+        <location filename="gui_ui.py" line="241"/>
+        <source>64</source>
+        <translation>64</translation>
+    </message>
+    <message>
+        <location filename="gui_ui.py" line="242"/>
+        <source>128</source>
+        <translation>128</translation>
+    </message>
+    <message>
+        <location filename="gui_ui.py" line="243"/>
+        <source>256</source>
+        <translation>256</translation>
+    </message>
+    <message>
+        <location filename="gui_ui.py" line="244"/>
+        <source>512</source>
+        <translation>512</translation>
+    </message>
+    <message>
+        <location filename="gui_ui.py" line="245"/>
+        <source>1024</source>
+        <translation>1024</translation>
+    </message>
+    <message>
+        <location filename="gui_ui.py" line="246"/>
+        <source>2048</source>
+        <translation>2048</translation>
+    </message>
+    <message>
+        <location filename="gui_ui.py" line="247"/>
+        <source>4086</source>
+        <translation>4086</translation>
+    </message>
+    <message>
+        <location filename="gui_ui.py" line="248"/>
+        <source>640x480</source>
+        <translation>640x480</translation>
+    </message>
+    <message>
+        <location filename="gui_ui.py" line="249"/>
+        <source>800x600</source>
+        <translation>800x600</translation>
+    </message>
+    <message>
+        <location filename="gui_ui.py" line="250"/>
+        <source>1024x768</source>
+        <translation>1024x768</translation>
+    </message>
+    <message>
+        <location filename="gui_ui.py" line="251"/>
+        <source>SquashFS format</source>
+        <translation>SquashFS 형식</translation>
+    </message>
+    <message>
+        <location filename="gui_ui.py" line="252"/>
+        <source>xz</source>
+        <translation>xz</translation>
+    </message>
+    <message>
+        <location filename="gui_ui.py" line="253"/>
+        <source>gzip</source>
+        <translation>gzip</translation>
+    </message>
+    <message>
+        <location filename="gui_ui.py" line="254"/>
+        <source>Settings</source>
+        <translation>세팅</translation>
+    </message>
+    <message>
+        <location filename="gui_ui.py" line="255"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;set on runtime!&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui_ui.py" line="268"/>
+        <source>About</source>
+        <translation>이 프로그램에 대해서</translation>
+    </message>
+    <message>
+        <location filename="gui_ui.py" line="231"/>
+        <source> Extra Customization </source>
+        <translation>커스터마이징 추가</translation>
+    </message>
+    <message>
+        <location filename="gui_ui.py" line="232"/>
+        <source>Browse FileSystem</source>
+        <translation>파일시스템 탐색</translation>
+    </message>
+    <message>
+        <location filename="gui_ui.py" line="233"/>
+        <source>Browse ISO</source>
+        <translation>ISO 탐색</translation>
+    </message>
+    <message>
+        <location filename="gui_ui.py" line="234"/>
+        <source>Extra</source>
+        <translation>추가</translation>
+    </message>
+    <message>
+        <location filename="gui_ui.py" line="256"/>
+        <source>Ivailo Monev &apos;SmiL3y&apos; (code developer) xakepa10@gmail.com
+Michal Glowienka &apos;eloaders&apos; (PPA maintainer) eloaders@yahoo.com
+Mubiin Kimura &apos;clearkimura&apos; (documentation) clearkimura@gmail.com
+Andrie Vorster &apos;andries-petrus-vorster&apos; (README.md) Ayman &apos;aymanim&apos; (typo, spellcheck)
+Yes &apos;eByrd&apos; (typo, spellcheck)
+
+If you feel like you deserve to be mentioned here but you are not feel free to contact us.
+
+The GNU General Public License version 2 (GPLv2)
+
+Copyright (C) 2010-2013 Ivailo Monev
+Copyright (C) 2013-2015 Mubiin Kimura</source>
+        <translation>Ivailo Monev &apos;SmiL3y&apos; (code developer) xakepa10@gmail.com
+Michal Glowienka &apos;eloaders&apos; (PPA maintainer) eloaders@yahoo.com
+Mubiin Kimura &apos;clearkimura&apos; (documentation) clearkimura@gmail.com
+Andrie Vorster &apos;andries-petrus-vorster&apos; (README.md) Ayman &apos;aymanim&apos; (typo, spellcheck)
+Yes &apos;eByrd&apos; (typo, spellcheck)
+
+minwook-shin / https://github.com/minwook-shin (korean translation) 
+
+If you feel like you deserve to be mentioned here but you are not feel free to contact us.
+
+The GNU General Public License version 2 (GPLv2)
+
+Copyright (C) 2010-2013 Ivailo Monev
+Copyright (C) 2013-2015 Mubiin Kimura</translation>
+    </message>
+</context>
+<context>
+    <name>app</name>
+    <message>
+        <location filename="gui.py" line="41"/>
+        <source>Information</source>
+        <translation>안내</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="44"/>
+        <source>Warning</source>
+        <translation>주의</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="47"/>
+        <source>Critical</source>
+        <translation>치명적</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="60"/>
+        <source>An instance of Customizer is already running.</source>
+        <translation>커스터마이저의 한 예가 이미 실행중입니다.</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="67"/>
+        <source>You are attempting to run Customizer with Python 3.</source>
+        <translation>당신은 파이썬 3으로 커스터마이저를 실행하려고합니다.</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="145"/>
+        <source>The filesystem is not valid or corrupted. Clean is recommended.</source>
+        <translation>파일시스템이 잘못되거나 손상되었습니다. 청소를 하는 것이 좋습니다.</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="166"/>
+        <source>No supported terminal emulator detected.</source>
+        <translation>지원되는 터미널 에뮬레이터가 감지 되지 않습니다.</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="228"/>
+        <source>Open</source>
+        <translation>열기</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="228"/>
+        <source>ISO Files (*.iso);;All Files (*)</source>
+        <translation>ISO 파일 (*.iso);;모든 파일 (*)</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="261"/>
+        <source>No supported text editor detected.</source>
+        <translation>지원되는 텍스트 에디터가 감지 되지 않습니다.</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="274"/>
+        <source>Deb Files (*.deb);;All Files (*)</source>
+        <translation>Deb 파일 (*.deb);;모든 파일 (*)</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="290"/>
+        <source>Shell Scripts (*.sh);;All Files (*)</source>
+        <translation>쉘 스크립트 (*.sh);;모든 파일 (*)</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="374"/>
+        <source>No supported file manager detected.</source>
+        <translation>지원되는 파일 탐색기가 감지 되지 않습니다.</translation>
+    </message>
+    <message>
+        <location filename="gui.py" line="320"/>
+        <source>Live user can not be empty.</source>
+        <translation type="unfinished">라이브 유저는 비워 둘 수 없습니다 .</translation>
+    </message>
+</context>
+</TS>
